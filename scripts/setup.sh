b927593ec453313df56e5e69bdc73eac234df0b2
Artur Zelik <artur.zelik@stud.tu-darmstadt.de>
2026-03-17 22:09:47 +0100

    Add dynamixel to setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,15 +12,32 @@ fi
 if [ ! -d "franka_ros2" ]; then
     echo "Cloning franka_ros2 (humble)..."
     # git clone -b humble https://github.com/frankarobotics/franka_ros2.git ./franka_ros2
-    git clone https://github.com/frankarobotics/franka_ros2 ./franka_ros2
+    git clone -b ${ROS_DISTRO} https://github.com/frankarobotics/franka_ros2 ./franka_ros2
     # Remove useless ros2 nodes from franka
     rm -rf ./franka_ros2/franka_gazebo ./franka_ros2/franka_fr3_moveit_config ./franka_ros2/franka_mobile_example_controllers ./franka_ros2/franka_mobile_sensors ./franka_ros2/franka_gazebo_bringup
     rm -rf ./franka_ros2/libfranka ./franka_ros2/franka_robot_state_broadcaster
     cd ./franka_ros2/ && git clone --recurse-submodules git@git.ias.informatik.tu-darmstadt.de:ros2/franka/libfranka.git
     cd ./franka_ros2/ && git clone git@git.ias.informatik.tu-darmstadt.de:ros2/franka/franka_robot_state_broadcaster.git
+    vcs import ./franka_ros2 < ./franka_ros2/dependency.repos --recursive --skip-existing
+    rosdep install --from-paths ./franka_ros2 --ignore-src --rosdistro humble --skip-keys "ignition-plugin franka_ign_ros2_control" -y
 else
     echo "franka_ros2 folder already exists, skipping clone."
 fi
 
-vcs import ./franka_ros2 < ./franka_ros2/dependency.repos --recursive --skip-existing
-rosdep install --from-paths ./franka_ros2 --ignore-src --rosdistro humble --skip-keys "ignition-plugin franka_ign_ros2_control" -y
+if [ ! -d "dynamixel_hardware_interface" ]; then
+    git clone -b ${ROS_DISTRO} https://github.com/ROBOTIS-GIT/dynamixel_hardware_interface.git ./dynamixel_hardware_interface
+else
+    echo "dynamixel_hardware_interface folder already exists, skipping clone."
+fi
+
+if [ ! -d "DynamixelSDK" ]; then
+    git clone -b ${ROS_DISTRO} https://github.com/ROBOTIS-GIT/DynamixelSDK.git ./DynamixelSDK
+else
+    echo "DynamixelSDK folder already exists, skipping clone."
+fi
+
+if [ ! -d "dynamixel_interfaces" ]; then
+    git clone -b ${ROS_DISTRO} https://github.com/ROBOTIS-GIT/dynamixel_interfaces.git ./dynamixel_interfaces
+else
+    echo "dynamixel_interfaces folder already exists, skipping clone."
+fi
