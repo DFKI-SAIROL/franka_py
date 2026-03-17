806977f514b7de3a23554a7c17013b5d4ec59b7f
Ken-Joel <ken-joel.simmoteit@tu-darmstadt.de>
2026-03-17 22:44:41 +0100

    Fix run robot task

--- scripts/run_robot.sh
+++ scripts/run_robot.sh
@@ -11,11 +11,9 @@ fi
 
 # Use tmux to open multiple terminals side-by-side
 SESSION="robot_run"
-tmux new-session -d -s $SESSION "ros2 launch franka_launch example.launch.py spawn_franka_left:=false spawn_franka_right:=true use_fake_hardware:=false"
-tmux split-window -h -t $SESSION "ssh jetson 'cd Projects/ros2_ws && bash launch_zed.sh'"
-tmux split-window -v -t $SESSION "ros2 launch zed_rig_aggregator_node aggregator.launch.py"
+tmux new-session -d -s $SESSION "pixi run -e humble ros2 launch franka_launch example.launch.py spawn_franka_left:=false spawn_franka_right:=true use_fake_hardware:=false ; exec bash"
+tmux split-window -h -t $SESSION "env -i HOME=$HOME USER=$USER /usr/bin/ssh -t jetson 'cd Projects/ros2_ws && source install/setup.bash && bash launch_zed.sh csil'" # choose config csil|max|"custom"
+tmux split-window -v -t $SESSION "pixi run -e humble ros2 launch zed_rig_aggregator_node aggregator.launch.py ; exec bash"
 
 echo "Started robot launch nodes in a tmux session."
-if [ -t 1 ]; then
-    tmux attach-session -t $SESSION
-fi
+tmux attach-session -t $SESSION
