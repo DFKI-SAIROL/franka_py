1eb79468ee483ce1d7f40437dcd260acd732270f
Artur Zelik <artur.zelik@stud.tu-darmstadt.de>
2026-03-18 19:35:59 +0100

    Fix ssh into jetson

--- scripts/run_robot.sh
+++ scripts/run_robot.sh
@@ -12,7 +12,7 @@ fi
 # Use tmux to open multiple terminals side-by-side
 SESSION="robot_run"
 tmux new-session -d -s $SESSION "pixi run -e humble ros2 launch franka_launch example.launch.py spawn_franka_left:=false spawn_franka_right:=true use_fake_hardware:=false ; exec bash"
-tmux split-window -h -t $SESSION "env -i HOME=$HOME USER=$USER /usr/bin/ssh -t jetson 'cd Projects/ros2_ws && source install/setup.bash && bash launch_zed.sh csil'" # choose config csil|max|"custom"
+tmux split-window -h -t $SESSION "env -i HOME=$HOME USER=$USER /usr/bin/ssh -t jetson 'cd Projects/ros2_ws && source install/setup.bash && export ROS_DOMAIN_ID=$ROS_DOMAIN_ID && bash launch_zed.sh csil'" # choose config csil|max|"custom"
 tmux split-window -v -t $SESSION "pixi run -e humble ros2 launch zed_rig_aggregator_node aggregator.launch.py ; exec bash"
 
 echo "Started robot launch nodes in a tmux session."
